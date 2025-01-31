1c8ac28b08e1f2fa4bfb92e60acb35ab8e514d8f
Cyber Bull <163235130+Cyber-Bulls@users.noreply.github.com>
2025-02-01 01:31:33 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ export default function Navbar() {
   ]
 
   return (
-    <nav className="fixed w-full bg-black/10 backdrop-blur-sm z-50 shadow-sm">
+    <nav className="fixed w-full bg-grey/100 backdrop-blur-sm z-50 shadow-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-20">
           <div className="flex-shrink-0">
