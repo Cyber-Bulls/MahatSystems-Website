fbcf15196edf4ab473eadcaf318d595fda0b2225
Cyber Bull <163235130+Cyber-Bulls@users.noreply.github.com>
2025-02-01 02:01:13 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,6 +17,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-20">
           <div className="flex-shrink-0">
+             <img src="assets/logo/logo1.png" alt="Mahat Systems Logo" className="h-8 w-8 mr-2" />
             <h1 className="text-2xl font-bold tracking-wider">MAHAT SYSTEMS</h1>
           </div>
           
